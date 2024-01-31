c608292674fff23ef041fa90c14459af67758599
2yeram <ramramm_@naver.com>
2024-01-31 11:28:25 +0900

    Fix: 액세스 차단  해결

--- front/fit-friends/WebViewScreen.js
+++ front/fit-friends/WebViewScreen.js
@@ -22,6 +22,7 @@ const WebViewScreen = () => {
         url ? (
             <WebView
                 source={{ uri: url }}
+                userAgent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
                 style={{ flex: 1 }}
             />
         ) : null
