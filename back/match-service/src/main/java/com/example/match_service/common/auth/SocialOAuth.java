9b59e866b81eb744eb41a42a3ce2eb67d6be885a
bokyeong <wntjdals0412@gmail.com>
2025-02-14 11:18:59 +0900

    feat: common 기능

---
+++ back/match-service/src/main/java/com/example/match_service/common/auth/SocialOAuth.java
@@ -0,0 +1,5 @@
+package com.example.match_service.common.auth;
+
+public interface SocialOAuth {
+    String getUserInfo(String code);
+}
---
+++ back/match-service/src/main/java/com/example/match_service/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.example.match_service.common.exception;
+
+
+import com.example.userservice.common.dto.CustomResponseBody;
+import com.example.userservice.common.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CustomResponseBody<Void>> handleException(Exception ex) {
+        if (ex.getMessage() != null) {
+            return ResponseUtil.error(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+
+        }
+        return ResponseUtil.error(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<CustomResponseBody<Void>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        if (ex.getMessage() != null) {
+            return ResponseUtil.error(HttpStatus.BAD_REQUEST, ex.getMessage());
+        }
+        return ResponseUtil.error(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<CustomResponseBody<Void>> UserNotFoundException(UserNotFoundException ex) {
+        if (ex.getMessage() != null) {
+            return ResponseUtil.error(HttpStatus.NOT_FOUND, ex.getMessage());
+
+        }
+        return ResponseUtil.error(HttpStatus.NOT_FOUND);
+    }
+}
