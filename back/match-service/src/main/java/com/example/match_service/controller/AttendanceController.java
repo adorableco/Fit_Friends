dd0f098d9ed76ffd2edac5f8b7e2d459fb928933
bokyeong <wntjdals0412@gmail.com>
2025-02-16 22:39:29 +0900

    FITFRND-24: common 코드 업데이트 및 controller userId 가져오는 기능

--- back/match-service/src/main/java/com/example/match_service/controller/AttendanceController.java
+++ back/match-service/src/main/java/com/example/match_service/controller/AttendanceController.java
@@ -2,6 +2,7 @@ package com.example.match_service.controller;
 
 import com.example.match_service.common.dto.CustomResponseBody;
 import com.example.match_service.common.dto.StatusCode;
+import com.example.match_service.common.resolver.userid.UserId;
 import com.example.match_service.service.AttendanceService;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +19,12 @@ public class AttendanceController {
     private final AttendanceService attendanceService;
 
     @GetMapping("/attendances/{matchId}")
-    public ResponseEntity<CustomResponseBody<byte[]>> createQr(@PathVariable Long matchId) throws Exception {
-        //TODO 헤더로부터 userID 추출
-        //우선은 임시값 설정
-        UUID userId = UUID.fromString("4d68c186-5ae4-4cde-a9c8-549c71f46478");
+    public ResponseEntity<CustomResponseBody<byte[]>> createQr(@UserId UUID userId, @PathVariable Long matchId) throws Exception {
         return ResponseEntity.ok(new CustomResponseBody<>(StatusCode.SUCCESS,"QR 생성에 성공하였습니다.",attendanceService.createQr(userId,matchId)));
     }
 
     @PostMapping("/attendances/{matchId}")
-    public ResponseEntity<CustomResponseBody<Void>> qrToAttendance(@PathVariable Long matchId) throws Exception {
-        UUID userId = UUID.fromString("4d68c186-5ae4-4cde-a9c8-549c71f46478");
+    public ResponseEntity<CustomResponseBody<Void>> qrToAttendance(@UserId UUID userId, @PathVariable Long matchId) throws Exception {
         attendanceService.qrToAttendance(userId,matchId);
         return ResponseEntity.ok(new CustomResponseBody<>(StatusCode.SUCCESS, "QR 출석에 성공하였습니다"));
     }
--- back/match-service/src/main/java/com/example/match_service/controller/MatchController.java
+++ back/match-service/src/main/java/com/example/match_service/controller/MatchController.java
@@ -2,6 +2,7 @@ package com.example.match_service.controller;
 
 import com.example.match_service.common.dto.CustomResponseBody;
 import com.example.match_service.common.dto.StatusCode;
+import com.example.match_service.common.resolver.userid.UserId;
 import com.example.match_service.service.ParticipationService;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,9 @@ public class MatchController {
     private ParticipationService participationService;
 
     @PostMapping("/participations/{matchId}")
-    ResponseEntity<CustomResponseBody<Void>> applyMatch(@PathVariable Long matchId){
+    ResponseEntity<CustomResponseBody<Void>> applyMatch(@UserId UUID userId, @PathVariable Long matchId){
         //TODO 헤더로부터 userID 추출
         //우선은 임시값 설정
-        UUID userId = UUID.fromString("4d68c186-5ae4-4cde-a9c8-549c71f46478");
         participationService.applyToMatch(matchId,userId);
         return ResponseEntity.ok(
                 new CustomResponseBody<>(StatusCode.SUCCESS, "참가 신청되었습니다."));
