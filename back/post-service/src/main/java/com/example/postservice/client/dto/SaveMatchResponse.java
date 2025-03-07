f0ba32c1a659a4bbc1e0313270688c0f9474aaf4
adorableco <tpdusdl1001@knu.ac.kr>
2025-03-08 01:10:37 +0900

    FITFRND-28 post-service와 타 마이크로서비스 연결

---
+++ back/post-service/src/main/java/com/example/postservice/client/dto/SaveMatchResponse.java
@@ -0,0 +1,8 @@
+package com.example.postservice.client.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SaveMatchResponse {
+    private Long matchId;
+}
--- back/post-service/src/main/java/com/example/postservice/service/PostService.java
+++ back/post-service/src/main/java/com/example/postservice/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.postservice.service;
 import com.example.postservice.client.MatchServiceClient;
 import com.example.postservice.client.UserServiceClient;
 import com.example.postservice.client.dto.MatchResponse;
+import com.example.postservice.client.dto.SaveMatchRequest;
 import com.example.postservice.client.dto.UserResponse;
 import com.example.postservice.common.exception.PostNotFoundException;
 import com.example.postservice.domain.Post;
@@ -15,6 +16,7 @@ import com.example.postservice.repository.PostRepository;
 import com.example.postservice.repository.TagRepository;
 import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,8 +24,10 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class PostService {
     private final PostRepository postRepository;
     private final TagRepository tagRepository;
@@ -37,6 +41,7 @@ public class PostService {
 
         return posts.stream().map(post -> PostResponse.builder()
                 .title(post.getTitle())
+                .category(post.getCategory())
                 .createdDate(post.getCreatedDate())
                 .modifiedDate(post.getModifiedDate())
                 .tag(post.getTag())
@@ -57,6 +62,7 @@ public class PostService {
                 .title(post.getTitle())
                 .createdDate(post.getCreatedDate())
                 .modifiedDate(post.getModifiedDate())
+                .category(post.getCategory())
                 .tag(post.getTag())
                 .userName(user.getName())
                 .userImage(user.getPicture())
@@ -64,10 +70,15 @@ public class PostService {
                 .build();
     }
 
+    @Transactional
     public PostIdResponse save(AddPostRequest dto, UUID userId) {
         Tag tag = dto.tagToEntity();
         Tag savedTag = tagRepository.save(tag);
-        Long matchId = matchServiceClient.saveMatch(dto.getMatch());
+        Long matchId = matchServiceClient.saveMatch(
+                new SaveMatchRequest(
+                        userId, dto.getCategory(), dto.getMatch().getPlace(), dto.getMatch().getStartTime(), dto.getMatch().getEndTime(), dto.getMatch().getHeadCnt()
+                )
+        );
 
         Post post = dto.postToEntity(savedTag,matchId,userId);
         Post saved = postRepository.save(post);
@@ -75,6 +86,7 @@ public class PostService {
         return new PostIdResponse(saved.getPostId());
     }
 
+    @Transactional
     public void deleteById(UUID userId , Long id){
         if ((postRepository.findById(id).orElseThrow(PostNotFoundException::new)).getUserId().equals(userId)) {
             postRepository.deleteById(id);
