cb0974059f76cf06d6e15c21e0ec88718f14982c
adorableco <tpdusdl1001@knu.ac.kr>
2025-03-14 15:43:41 +0900

    FITFRND-48 PR 등록 후 1시간 이후에만 coderabbit 리뷰 사용

--- dist/index.js
+++ dist/index.js
@@ -30000,7 +30000,7 @@ async function run() {
         for (const pull of pulls) {
             const createdAt = new Date(pull.created_at);
             const diffInHours = (now.getTime() - createdAt.getTime()) / (1000 * 60 * 60);
-            if (diffInHours <= 1) {
+            if (diffInHours >= 1) {
                 const { data: reviews } = await octokit.rest.pulls.listReviews({
                     owner,
                     repo,
--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ async function run() {
       const createdAt = new Date(pull.created_at);
       const diffInHours = (now.getTime() - createdAt.getTime()) / (1000 * 60 * 60);
       
-      if (diffInHours <= 1) {
+      if (diffInHours >= 1) {
         const { data: reviews } = await octokit.rest.pulls.listReviews({
           owner,
           repo,
