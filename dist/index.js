a728221c46ac3be680ee0fcf8dbc864ce65e3c06
adorableco <tpdusdl1001@knu.ac.kr>
2025-03-14 14:58:01 +0900

    FITFRND-48 수정 후 새 빌드 폴더

--- dist/index.js
+++ dist/index.js
@@ -29975,8 +29975,8 @@ var __importStar = (this && this.__importStar) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(7484));
-const github = __importStar(__nccwpck_require__(3228));
 const api_1 = __nccwpck_require__(6879);
+const initialize_1 = __nccwpck_require__(6649);
 const updateLabel = async (number) => {
     return (0, api_1.addLabels)(number)
         .then(() => {
@@ -29990,15 +29990,10 @@ const updateLabel = async (number) => {
 };
 async function run() {
     try {
-        const token = process.env.GITHUB_TOKEN;
-        if (!token) {
-            throw new Error('GITHUB_TOKEN이 설정되지 않았습니다.');
-        }
-        const octokit = github.getOctokit(token);
-        const { owner, repo } = github.context.repo;
+        (0, initialize_1.initialize)();
         const { data: pulls } = await octokit.rest.pulls.list({
-            owner,
-            repo,
+            owner: global.owner,
+            repo: global.repo,
             state: 'open'
         });
         const now = new Date();
@@ -30032,6 +30027,60 @@ async function run() {
     }
 }
 run();
+
+
+/***/ }),
+
+/***/ 6649:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.initialize = void 0;
+const core = __importStar(__nccwpck_require__(7484));
+const github = __importStar(__nccwpck_require__(3228));
+const initialize = () => {
+    global.owner = github.context.repo.owner;
+    global.repo = github.context.repo.repo;
+    const token = core.getInput("token", { required: true });
+    const octokit = github.getOctokit(token);
+    global.octokit = octokit;
+};
+exports.initialize = initialize;
 
 
 /***/ }),
