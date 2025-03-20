6aa40c8f7a31a3a921f15dc21f3171b3548fa532
adorableco <tpdusdl1001@knu.ac.kr>
2025-03-20 17:23:02 +0900

    docs: user-service smart-doc

---
+++ back/user-service/src/main/resources/static/search.js
@@ -0,0 +1,208 @@
+let api = [];
+const apiDocListSize = 1
+api.push({
+    name: 'default',
+    order: '1',
+    list: []
+})
+api[0].list.push({
+    alias: 'AuthController',
+    order: '17',
+    link: 'auth',
+    desc: 'Auth',
+    list: []
+})
+api[0].list[0].list.push({
+    order: '1',
+    deprecated: 'false',
+    url: '/login/{code}',
+    methodId: '80f30d622738ab52efc7c4c634c4842e',
+    desc: '소셜 로그인 후 사용자 정보를 요청하여 처리합니다.',
+});
+api[0].list.push({
+    alias: 'UserController',
+    order: '18',
+    link: 'user',
+    desc: 'User',
+    list: []
+})
+api[0].list[1].list.push({
+    order: '1',
+    deprecated: 'false',
+    url: '/signup',
+    methodId: 'bd710c0a529e16bc1254d1bddae5ea03',
+    desc: '사용자 회원가입 처리',
+});
+api[0].list[1].list.push({
+    order: '2',
+    deprecated: 'false',
+    url: '/users/{userId}',
+    methodId: 'dc832a2d4e91ac752b5fc1846b59c982',
+    desc: '특정 사용자의 상세 정보 조회',
+});
+api[0].list[1].list.push({
+    order: '3',
+    deprecated: 'false',
+    url: '/users/game-results',
+    methodId: 'a5457489b4b6b61f600b6430af6288c8',
+    desc: '게임 결과를 적용하여 사용자 정보 업데이트',
+});
+api[0].list[1].list.push({
+    order: '4',
+    deprecated: 'false',
+    url: '/users',
+    methodId: '4f5a5768449ae7dd78947dfd8aa90a1d',
+    desc: '사용자 상세 정보 수정',
+});
+document.onkeydown = keyDownSearch;
+function keyDownSearch(e) {
+    const theEvent = e;
+    const code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+    if (code === 13) {
+        const search = document.getElementById('search');
+        const searchValue = search.value.toLocaleLowerCase();
+
+        let searchGroup = [];
+        for (let i = 0; i < api.length; i++) {
+
+            let apiGroup = api[i];
+
+            let searchArr = [];
+            for (let i = 0; i < apiGroup.list.length; i++) {
+                let apiData = apiGroup.list[i];
+                const desc = apiData.desc;
+                if (desc.toLocaleLowerCase().indexOf(searchValue) > -1) {
+                    searchArr.push({
+                        order: apiData.order,
+                        desc: apiData.desc,
+                        link: apiData.link,
+                        alias: apiData.alias,
+                        list: apiData.list
+                    });
+                } else {
+                    let methodList = apiData.list || [];
+                    let methodListTemp = [];
+                    for (let j = 0; j < methodList.length; j++) {
+                        const methodData = methodList[j];
+                        const methodDesc = methodData.desc;
+                        if (methodDesc.toLocaleLowerCase().indexOf(searchValue) > -1) {
+                            methodListTemp.push(methodData);
+                            break;
+                        }
+                    }
+                    if (methodListTemp.length > 0) {
+                        const data = {
+                            order: apiData.order,
+                            desc: apiData.desc,
+                            link: apiData.link,
+                            alias: apiData.alias,
+                            list: methodListTemp
+                        };
+                        searchArr.push(data);
+                    }
+                }
+            }
+            if (apiGroup.name.toLocaleLowerCase().indexOf(searchValue) > -1) {
+                searchGroup.push({
+                    name: apiGroup.name,
+                    order: apiGroup.order,
+                    list: searchArr
+                });
+                continue;
+            }
+            if (searchArr.length === 0) {
+                continue;
+            }
+            searchGroup.push({
+                name: apiGroup.name,
+                order: apiGroup.order,
+                list: searchArr
+            });
+        }
+        let html;
+        if (searchValue === '') {
+            const liClass = "";
+            const display = "display: none";
+            html = buildAccordion(api,liClass,display);
+            document.getElementById('accordion').innerHTML = html;
+        } else {
+            const liClass = "open";
+            const display = "display: block";
+            html = buildAccordion(searchGroup,liClass,display);
+            document.getElementById('accordion').innerHTML = html;
+        }
+        const Accordion = function (el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+            const links = this.el.find('.dd');
+            links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown);
+        };
+        Accordion.prototype.dropdown = function (e) {
+            const $el = e.data.el;
+            let $this = $(this), $next = $this.next();
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp("20").parent().removeClass('open');
+            }
+        };
+        new Accordion($('#accordion'), false);
+    }
+}
+
+function buildAccordion(apiGroups, liClass, display) {
+    let html = "";
+    if (apiGroups.length > 0) {
+        if (apiDocListSize === 1) {
+            let apiData = apiGroups[0].list;
+            let order = apiGroups[0].order;
+            for (let j = 0; j < apiData.length; j++) {
+                html += '<li class="'+liClass+'">';
+                html += '<a class="dd" href="#' + apiData[j].alias + '">' + apiData[j].order + '.&nbsp;' + apiData[j].desc + '</a>';
+                html += '<ul class="sectlevel2" style="'+display+'">';
+                let doc = apiData[j].list;
+                for (let m = 0; m < doc.length; m++) {
+                    let spanString;
+                    if (doc[m].deprecated === 'true') {
+                        spanString='<span class="line-through">';
+                    } else {
+                        spanString='<span>';
+                    }
+                    html += '<li><a href="#' + doc[m].methodId + '">' + apiData[j].order + '.' + doc[m].order + '.&nbsp;' + spanString + doc[m].desc + '<span></a> </li>';
+                }
+                html += '</ul>';
+                html += '</li>';
+            }
+        } else {
+            for (let i = 0; i < apiGroups.length; i++) {
+                let apiGroup = apiGroups[i];
+                html += '<li class="'+liClass+'">';
+                html += '<a class="dd" href="#_'+apiGroup.order+'_' + apiGroup.name + '">' + apiGroup.order + '.&nbsp;' + apiGroup.name + '</a>';
+                html += '<ul class="sectlevel1">';
+
+                let apiData = apiGroup.list;
+                for (let j = 0; j < apiData.length; j++) {
+                    html += '<li class="'+liClass+'">';
+                    html += '<a class="dd" href="#' + apiData[j].alias + '">' +apiGroup.order+'.'+ apiData[j].order + '.&nbsp;' + apiData[j].desc + '</a>';
+                    html += '<ul class="sectlevel2" style="'+display+'">';
+                    let doc = apiData[j].list;
+                    for (let m = 0; m < doc.length; m++) {
+                       let spanString;
+                       if (doc[m].deprecated === 'true') {
+                           spanString='<span class="line-through">';
+                       } else {
+                           spanString='<span>';
+                       }
+                       html += '<li><a href="#' + doc[m].methodId + '">'+apiGroup.order+'.' + apiData[j].order + '.' + doc[m].order + '.&nbsp;' + spanString + doc[m].desc + '<span></a> </li>';
+                   }
+                    html += '</ul>';
+                    html += '</li>';
+                }
+
+                html += '</ul>';
+                html += '</li>';
+            }
+        }
+    }
+    return html;
+}
