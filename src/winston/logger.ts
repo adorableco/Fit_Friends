a0d2cb08b9eb60047ee32bca54310896e7d60c00
adorableco <tpdusdl1001@knu.ac.kr>
2025-03-25 21:59:24 +0900

    feat: winston logger 세팅

---
+++ src/winston/logger.ts
@@ -0,0 +1,35 @@
+const wiston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+const { combine, timestamp, label, printf } = wiston.format;
+
+const logDir = `logs`;
+
+const logFormat = printf(({ level, message, label, timestamp }: { level: string; message: string; label: string; timestamp: string }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = wiston.createLogger({
+    level: 'info',
+    format:combine(label({ label: 'AI_REVIEW' }), timestamp(), logFormat),
+    transports: [
+        new winstonDaily({
+            level : "info",
+            datePattern: 'YYYY-MM-DD-HH',
+            dirname: logDir,
+            filename: `%DATE%.log`,
+            maxSize: "20m",
+            maxFiles: "30d",
+        }),
+        new winstonDaily({
+            level: "error",
+            datePattern: 'YYYY-MM-DD',
+            dirname: logDir + '/error',
+            filename: `%DATE%.error.log`,
+            maxSize: "20m",
+            maxFiles: "30d",
+        })
+    ]
+});
+
+module.exports = logger;
+
